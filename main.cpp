50037583f1524dda2143052de153b4f2db80cca6
Patrick Coxall <mr.coxall@mths.ca>
2022-12-07 14:53:24 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 202Z XXX YYY
+//
+// Created by: XXX YYY
+// Created on: XXX YYY
+// This program ...
+
+#include <iostream>
+
+
+int main() {
+    // This function ...
+
+    std::cout << "\nDone." << std::endl;
+}
